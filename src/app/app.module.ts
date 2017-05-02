7a59e4d06c7e894749badca71de9ae1fd5487114
Charles Buczel <charles.buczel@gmail.com>
2017-05-02 18:30:00 +0100

    Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,47 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule }   from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent }  from './app.component';
-import { HeroDetailComponent } from './hero-detail.component'
+import { DashboardComponent } from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      }, {
+        path: 'heroes',
+        component: HeroesComponent
+      }, {
+        path: 'dashboard',
+        component: DashboardComponent
+      }, {
+        path: 'detail/:id',
+        component: HeroDetailComponent
+      }
+    ])
   ],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
   ],
-  bootstrap:    [ AppComponent ]
+  providers: [
+    HeroService
+  ],
+  bootstrap:    [
+    AppComponent
+  ]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -13,4 +13,8 @@ export class HeroService {
       setTimeout(() => resolve(this.getHeroes()), 2000)
     });
   }
+  getHero(id: number): Promise<Hero> {
+    return this.getHeroes()
+      .then(heroes => heroes.find(hero => hero.id === id));
+  }
 }
