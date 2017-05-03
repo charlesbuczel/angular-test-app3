42adb4c7a1a18e0b0408844424ab8c7953f175d6
Charles Buczel <charles.buczel@gmail.com>
2017-05-03 16:05:06 +0100

    HTTP

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -47,6 +47,15 @@ export class HeroService {
       .catch(this.handleError);
   }
 
+  delete(id: number): Promise<void> {
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete(url, { headers: this.headers })
+      .toPromise()
+      .then(() => null)
+      .catch(this.handleError)
+  }
+
   handleError(error: any): Promise<any> {
     return Promise.reject(error.message || error);
   }
