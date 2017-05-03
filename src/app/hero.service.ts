cb0b8c89eded13ccbb577345982e9f08faa51dcc
Charles Buczel <charles.buczel@gmail.com>
2017-05-03 13:56:57 +0100

    HTTP

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -8,6 +8,9 @@ import { Hero } from './hero';
 @Injectable()
 export class HeroService {
   private heroesUrl = 'api/heroes';
+  private headers = new Headers({
+    'Content-Type': 'application/json'
+  });
 
   constructor(private http: Http) { }
 
@@ -24,6 +27,23 @@ export class HeroService {
     return this.http.get(url)
       .toPromise()
       .then(response => response.json().data as Hero)
+      .catch(this.handleError);
+  }
+
+  update(hero: Hero): Promise<Hero> {
+    const url = `${this.heroesUrl}/${hero.id}`;
+
+    return this.http.put(url, JSON.stringify(hero), { headers: this.headers})
+      .toPromise()
+      .then(() => hero)
+      .catch(this.handleError);
+  }
+
+  create(name: string): Promise<Hero> {
+    return this.http
+      .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+      .toPromise()
+      .then(res => res.json().data as Hero)
       .catch(this.handleError);
   }
 
